4b19ec8cee5a9db2542ad36d702219b2a98f221a
Kim Dilling <kimdilling@gmail.com>
2015-09-11 10:26:25 -0500

    working model need to define function and finish readme

--- README.md
+++ README.md
@@ -1,30 +1,65 @@
 # tic_tac_toe
 Project 1
--styling note background color #6497b1
 
 
-Use canvas to create board - look at bookmarked references
 
+	Resource List:
+
+	Design Notes
+		http://www.dreamincode.net/forums/topic/301885-programming-a-js-game-part-1-design/
+
+		http://fundamentals.generalassemb.ly/05_chapter/05_lesson.html
+
+	Tic Tac Toe Examples
+		http://blog.modulus.io/building-tic-tac-together-game-logic
+
+		http://www.theodinproject.com/javascript-and-jquery/tic-tac-toe
+
+		http://www.dreamincode.net/forums/topic/247361-simple-tic-tac-toe-using-html5-css3-and-javascript/
+
+		http://jsfiddle.net/himanshuy/qTKkb/6/
+
+		http://www.dreamincode.net/forums/topic/296317-creating-a-simple-tic-tac-toe-game-in-javascript/
+
+		http://beesmart.github.io/JS_tictactoe/
+
+	Canvas Tutorial
+		https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+
+
+Initial Psuedocode:
 1. Create simple board with canvas in html file
 
-2. wWrk through game logic - 
-there is a 3x3 grid as a playing field
+2. Work through game logic - 
+	There is a 3x3 grid as a playing field
 
-3. There are 2 players
+	There are 2 players
 
-4. The players draw consecutively
-each player uses own symbol. The 1st player uses a cross (X) the 2nd player a circle (O)
+	The players draw consecutively
+	each player uses own symbol. The 1st player uses a cross (X) the 2nd player a circle (O)
 
-5. The current player (on-turn) must choose an empty field and mark it with his symbol
+	The current player (on-turn) must choose an empty field and mark it with his symbol
 
-6. A marked field cannot be chosen again
+	A marked field cannot be chosen again
 
-7. Ff a player reaches 3 symbols in a row or a column or a diagonal, they win. 
+	If a player reaches 3 symbols in a row or a column or a diagonal, they win. 
 
-9. This will end the game.
+	This will end the game.
 
-10. If neither player gets 3 successive fields and all fields are marked, the game ends with a draw. 
+	If neither player gets 3 successive fields and all fields are marked, the game ends with a draw. 
+
+	Text display win/tie to page element
 
 11. Add style elements
 
 12. animation? sound?
+
+Canvas Element Note
+	Each canvas is given a unique ID
+	
+	Onclick function of each canvas draws an element (X or O) on the canvas
+
+
+
+	**Canvas Style elements should be in CSS but they render as rectangles when I switch them over
+
--- gameplay.js
+++ gameplay.js
@@ -1,36 +1,33 @@
 
+var painted;//
+var content;//		
 
-		
-		var painted;
-		var content;
-		var winningCombinations;
-		var turn = 0;
-		var theCanvas;
-		var c;
-		var cxt;
-		var squaresFilled = 0;
-		var w;
-		var y;
-
-		
-		window.onload=function(){
+		//create the three arrays, set the winning combinations, start the iterater, clean board
+	window.onload=function(){
 			
-			painted = new Array();
-			content = new Array();
-			winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+		painted = new Array();
+		content = new Array();
+		winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-			for(var l = 0; l <= 8; l++){
-			painted[l] = false;
-			content[l]='';
+			for(var i = 0; i <= 8; i++){
+			painted[i] = false;
+			content[i]='';
 			}
-		}
+	}
 
-		//Game methods
-		function canvasClicked(canvasNumber){
-			theCanvas = "canvas"+canvasNumber;
-			c = document.getElementById(theCanvas);
-			cxt = c.getContext("2d");
+	var squaresFilled = 0;	
+	var c;
+	var cxt;
+	var theCanvas;//the box - canvas
+	var turn = 0;//who will play x or o
 
+	function canvasSelected(canvasNumber){
+
+		theCanvas = "canvas"+canvasNumber;
+		c = document.getElementById(theCanvas);
+		cxt = c.getContext("2d");
+			
+			//drawing method for X
 			if(painted[canvasNumber-1] ==false){
 				if(turn%2==0){
 					cxt.beginPath();
@@ -42,7 +39,7 @@
 					cxt.closePath();
 					content[canvasNumber-1] = 'X';
 				}
-
+				//drawing method for o
 				else{
 					cxt.beginPath();
 					cxt.arc(50,50,40,0,Math.PI*2,true);
@@ -50,45 +47,55 @@
 					cxt.closePath();
 					content[canvasNumber-1] = 'O';
 				}
+				//change player, play, check for winners
+			turn++;
+			painted[canvasNumber-1] = true;
+			squaresFilled++;
+			checkForWinners(content[canvasNumber-1]);
 
-				turn++;
-				painted[canvasNumber-1] = true;
-				squaresFilled++;
-				checkForWinners(content[canvasNumber-1]);
+			//tie game result, refresh
+			if(squaresFilled==9){
+				
+				$(".replyText").text("No Winner. Game Over. Refresh Page For New Game.");
 
-				if(squaresFilled==9){
-					alert("THE GAME IS OVER!");
-					location.reload(true);
-				}
+			}
 			
-			}
-			else{
-				alert("Space not available!");
-			}
+				} else	{
+
+					alert("Space Not Available");
+				}
 
 		}
 
-		function checkForWinners(symbol){
+	var winningCombinations;//possible wins
+	//determine winner
+	function checkForWinners(symbol){
 			
-			for(var a = 0; a < winningCombinations.length; a++){
+		for(var a = 0; a < winningCombinations.length; a++){
+
 			if(content[winningCombinations[a][0]]==symbol&&content[winningCombinations[a][1]]==	symbol&&content[winningCombinations[a][2]]==symbol){
-				alert(symbol+ " WON!");
+				$(".replyText").text(symbol+ " WON!");
 				playAgain();
 			}
 			}
 
 		}
 
-		function playAgain(){
-			y=confirm("PLAY AGAIN?");
+	function playAgain(){
+		var y=confirm("PLAY AGAIN?");  
+			
 			if(y==true){
+				
 				location.reload(true);
-			}
-			else{
-				alert("GoodBye!");
+
+			} else	{
+
+				$(".replyText").text("Goodbye");
+				//return alert("Goodbye");with div element replyText;
 		}
 
-		}
+	}
+
 
 
 	
